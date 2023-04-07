739365bf50548cf85f9e74334a2559f2eb1eb1fd
ukohank517 <ukohank517@gmail.com>
2023-04-07 23:18:35 +0900

    add config

--- eth_app/smart_contract/hardhat.config.js
+++ eth_app/smart_contract/hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "goerli",
   solidity: "0.8.4",
+  networks: {
+    goerli:{
+      url: "<alchemyapiのURL>",
+      accounts: [
+        "<ETH ウォレットの秘密鍵>"
+      ]
+    }
+  }
 };
